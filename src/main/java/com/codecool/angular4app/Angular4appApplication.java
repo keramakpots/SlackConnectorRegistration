2e3291ee321fd122034f3a7ae174514fb2dcfcbe
Marek Stopka <keramakpots@gmail.com>
2017-08-05 14:17:02 +0200

    app starter

---
+++ src/main/java/com/codecool/angular4app/Angular4appApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.angular4app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular4appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular4appApplication.class, args);
+	}
+}
